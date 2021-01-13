bf4b110f47ff3d13e9bb980938d22e784a6945bf
Vitor Serrano <vitorcirinoserrano@gmail.com>
2021-01-12 22:44:45 -0300

    :memo: Update hyper preferences

--- .hyper.js
+++ .hyper.js
@@ -15,9 +15,21 @@ module.exports = {
     bell: "SOUND",
     macOptionSelectionMode: "vertical",
     webGLRenderer: true,
+    paneNavigation: {
+      showIndicators: false,
+    },
   },
-  plugins: ["hyper-custom-controls"],
-  localPlugins: ["hyper-omni"],
+
+  plugins: [
+    "hyper-custom-controls",
+    "hypercwd",
+    "hyper-statusline",
+    "hyper-aura-theme",
+    "hyper-tabs-enhanced",
+    "hyper-pane",
+  ],
+  // localPlugins: ["hyper-omni"],
+
   hyperMacControls: {
     flipped: true,
   },
